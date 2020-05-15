986832cb47c1b130a23342176257d70bd4967481
Leonid-Panasiuk <lona.panasuk@gmail.com>
2020-05-16 00:22:39 +0300

    add

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -16,19 +16,20 @@ export class ListItemsComponent implements OnInit {
   ngOnInit() {
     this.products = this.productsService.getProducts();
   }
-  
+
   slides = [
-    {img: "/assets/images/slick-2.png", id:1},
-    {img: "/assets/images/slick-1.jpg", id:2},
-    {img: "/assets/images/slick-3.jpg", id:3}, 
+    { img: "/assets/images/slick-2.png", id: 1 },
+    { img: "/assets/images/slick-1.jpg", id: 2 },
+    { img: "/assets/images/slick-3.jpg", id: 3 },
   ];
-  slideConfig = {"slidesToShow": 1,
-   "slidesToScroll": 1,
-   "dots": true,
-   "infinite": true,
-  //  "autoplay": true,
-   arrows: true,
-  //  "autoplaySpeed": 1500
+  slideConfig = {
+    "slidesToShow": 1,
+    "slidesToScroll": 1,
+    "dots": true,
+    "infinite": true,
+    "autoplay": true,
+    arrows: true,
+    "autoplaySpeed": 1500
   };
-  
+
 }
